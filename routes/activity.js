42830de6a0ad33183640f96891d9cafa64986499
Ben563 <59427402+Ben563@users.noreply.github.com>
2020-12-01 16:05:40 +0800

    2020/12/01

--- routes/activity.js
+++ routes/activity.js
@@ -5,14 +5,81 @@ var util = require('util');
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var util = require('util');
-//var sync = require('synchronize');
+var schedule = require('node-schedule');
 
-// const { Client } = require('pg');
-// const client = new Client({
-// 	  connectionString: process.env.DATABASE_URL,
-// 	  ssl: true,
-// 	});
-// client.connect();
+const ET_Client     = require('sfmc-fuelsdk-node');
+const clientId      = "rnriw78wwxfrpdw9ss37wown";
+const clientSecret  = "BDNX68eeMORjV1a6fZcVku6s";
+const stack         = null;
+
+
+const origin              = 'https://mcjhmstdw76qsk9kk6m1zpwm4xf4.rest.marketingcloudapis.com/';
+const authOrigin          = 'https://mcjhmstdw76qsk9kk6m1zpwm4xf4.auth.marketingcloudapis.com/';
+const soapOrigin          = 'https://mcjhmstdw76qsk9kk6m1zpwm4xf4.soap.marketingcloudapis.com/';
+
+const client = new ET_Client(
+  clientId, 
+  clientSecret, 
+  stack, 
+  {
+    origin, 
+    authOrigin, 
+    soapOrigin, 
+    authOptions: { 
+      authVersion: 2, 
+      accountId: 7327915, 
+      scope: 'data_extensions_read data_extensions_write',
+      applicationType: 'server'
+    }
+  }
+);
+
+var offerID = "";
+var journeyID = '';
+var dataResult = {};
+var scheduleJobRetry=0;
+
+function retrieveDataFromDE(){
+
+    return new Promise((resolve, reject)=>{
+        console.log("retrieveDataFromDE function");
+        //retrieve from DataExtension
+        const deRow = client.dataExtensionRow({
+                //dataExtension which you want to retrieve from
+                Name: 'OfferDemo',
+                //field name
+                props: ['offerID', 'item1','item2'],
+                filter: {
+                    leftOperand: 'offerID',
+                    //operator includes : equals, notEquals, greaterThan, lessThan
+                    operator: 'equals',
+                    rightOperand: offerID
+                }
+                // to return all rows, delete the filter property
+        });
+
+        deRow.get((err, res) => {
+            if (err) {
+                console.error(err.message);
+            } 
+            else {
+                var temp = res.body.Results;
+                if(!temp==""){
+                    for (const result of res.body.Results) {
+                        for (const property of result.Properties.Property) {
+                            var nameStr= property.Name;
+                            var valueStr = property.Value;
+                            dataResult.nameStr = valueStr
+                        }
+                    }
+                }
+            }
+        });
+        console.log("return dataResult==>"+JSON.stringify(dataResult));
+        resolve();
+    });
+}
+
 
 exports.logExecuteData = [];
 
@@ -93,12 +160,41 @@ exports.execute = function (req, res) {
         }
 
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-            
+            var map = {};
+            journeyID = decoded.journeyId;
             // decoded in arguments
             var decodedArgs = decoded.inArguments[0];            
             logData(req);
 			console.log( JSON.stringify(  decodedArgs  ));
 			console.log( JSON.stringify(  decoded  ));
+            for(var i in decoded.inArguments){
+                var startDate = decoded.inArguments[i].OfferStartDate;
+                var endDate = decoded.inArguments[i].OfferExpiryDate;
+                var offerID = decoded.inArguments[i].OfferID;
+                if(offerID!=null){
+                    map.offerID = offerID;
+                }
+                else if(startDate!=null){
+                    map.startDate = startDate;
+                }
+                else if(endDate!=null){
+                    map.endDate = endDate;
+                }
+                else if(name != null){
+                    map.name = name;
+                }
+                else if(Email!=null){
+                    map.Email = Email;
+                }
+            }
+            var isEmpty = JSON.stringify(map)=="{}";
+            if(isEmpty!=true){
+                // map.journeyid = journeyID;
+                // map.status = 'pending';
+                // var queryStr = 'INSERT INTO ben.offer(name,email,startdate,enddate,,journeyid,status,createdate,offerid,item1,item2) VALUES($1::varchar, $2::varchar,$3::varchar,$5::varchar,$6::varchar,$7::varchar,$8::varchar,$9::varchar,$10::varchar,$11::varchar)';
+                // var parameters = [map.name,map.Email,map.startDate,map.endDate,map.journeyid,map.status,dateFormat(new Date()),map.offerID,];
+                // insertDataIntoDB(queryStr,parameters);
+            }
             res.send(200, 'Execute');
         } else {
             console.error('inArguments invalid.');
@@ -112,9 +208,14 @@ exports.execute = function (req, res) {
  * POST Handler for /publish/ route of Activity.
  */
 exports.publish = function (req, res) {
-    // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
-    logData(req);
+    console.log('publish module');
+    //var rule = '0 '+mm+' '+h+' '+d+' '+m+' *';
+    var rule = '0/1 * * * * *';
+    console.log("rule==>"+rule);
+    //reset 
+    scheduleJobRetry = 0;
+    setScheduleJob(rule,retrieveDataFromDB);
+    
     res.send(200, 'Publish');
 };
 
@@ -132,3 +233,25 @@ exports.validate = function (req, res) {
 exports.resolveToken = function (req, res) {
  
 };
+
+function setScheduleJob(rule,retrieveDataFromDB){
+    console.log("start scheduleJob");
+    var j = schedule.scheduleJob(rule,function(){
+        console.log("schedule Job Starting");
+        console.log("retrySchedule==>"+scheduleJobRetry);
+        if(scheduleJobRetry>=3){
+            console.log("stop schedule");
+            console.log("stop database server connection");
+            j.cancel();
+        }
+        else{
+            retrieveDataFromDE().then(function(){
+                retrieveDataFromDB();
+            });
+        }
+    });
+}
+
+function retrieveDataFromDB(){
+    console.log("retrieveDataFromDB function");
+}
