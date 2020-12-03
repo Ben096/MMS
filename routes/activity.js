20c21301194de78a26465235b2a1f3dfa92c7e90
Ben563 <59427402+Ben563@users.noreply.github.com>
2020-12-03 09:50:10 +0800

    2020/11/03

--- routes/activity.js
+++ routes/activity.js
@@ -138,7 +138,7 @@ function logData(req) {
  */
 exports.edit = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
-    //console.log( req.body );
+    console.log("Edit");
     res.send(200, 'Edit');
 };
 
