85bf5b26ae9e82d03d34d542e7cc293b1e1c4063
Ben563 <59427402+Ben563@users.noreply.github.com>
2020-12-03 10:20:18 +0800

    2020/12/03

--- routes/activity.js
+++ routes/activity.js
@@ -225,7 +225,7 @@ exports.publish = function (req, res) {
     var f = nowDate.getMinutes();
     var mm = parseInt(f)+5;
     //var rule = '0 '+mm+' '+h+' '+d+' '+m+' *';
-    var rule = '0 0/5,5/5 * * * *';
+    var rule = '0 5/5,59 * * * *';
     console.log("rule==>"+rule);
     //reset 
     scheduleJobRetry = 0;
