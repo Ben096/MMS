4f434ba9a39a78ba63d8bbce34f9bd3472327e2c
Ben096 <ruibin096@gmail.com>
2022-10-23 16:09:03 +0800

    Update

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -8,74 +8,16 @@ define([
     var connection = new Postmonger.Session();
     var authTokens = {};
     var payload = {};
-
-    var fieldArr = [];
-
-    var steps = [
-        {'key': 'step1', 'label': 'Step1'},
-        {'key': 'step2', 'label': 'Step2'}
-    ];
-    var currentStep = steps[0].key;
-
-    var globalVariable="";
+    
+    
 
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
-
-    //running modal
-    connection.on('initActivityRunningModal', initRunningModal);
-    //hover modal
-    connection.on('initActivityRunningHover', initRunningModal);
-
     connection.on('requestedTokens', onGetTokens);
     connection.on('requestedEndpoints', onGetEndpoints);
 
-    //click Next & Previous button
-    connection.on('clickedNext', onClickedNext);
-    connection.on('clickedBack', onClickedBack);
-    connection.on('gotoStep', onGotoStep);
-
-
-    function onClickedNext () {
-        if (currentStep.key === 'summary') {
-            save();
-        } else {
-            connection.trigger('nextStep');
-        }
-    }
-
-    function onClickedBack () {
-        connection.trigger('prevStep');
-    }
-
-    function onGotoStep (step) {
-        showStep(step);
-        connection.trigger('ready');
-    }
-
-    function showStep (step, stepIndex) {
-        console.log("showStep function");
-        console.log("step==>"+step);
-        console.log("stepIndex==>"+stepIndex);
-        if (stepIndex && !step) {
-            step = steps[stepIndex - 1];
-        }
-
-        currentStep = step;
-
-        $('.step').hide();
-
-        switch (currentStep.key) {
-        case 'step1':
-            $('#step1').show();
-            break;
-        case 'step2':
-            $('#step2').show();
-            break;
-        }
-    }
-
+    connection.on('clickedNext', save);
    
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
@@ -83,33 +25,42 @@ define([
 
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
-        console.log ('onRender function');
+        console.log ('changed btn33');
         
     }
 
     function initialize(data) {
-        console.log("init==>"+JSON.stringify(data));
+        console.log('do initialize');
+        console.log(data);
         if (data) {
             payload = data;
-            //init 
-            initOperation(data);
         }
+        
+        var hasInArguments = Boolean(
+            payload['arguments'] &&
+            payload['arguments'].execute &&
+            payload['arguments'].execute.inArguments &&
+            payload['arguments'].execute.inArguments.length > 0
+        );
 
-       console.log('initActivity function');
-    }
+        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-    function initRunningModal(data){
-        console.log("initRunningModal function==>"+JSON.stringify(data));
-        if (data) {
-            initOperation(data);
-        }
-    }
+        console.log(inArguments);
 
-    function initRunningHover(data){
-        console.log("initRunningHover function==>"+JSON.stringify(data));
-        if (data) {
-            initOperation(data);
-        }
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                
+              
+            });
+        });
+
+
+ // connection.trigger('updateButton', {
+ //            button: 'next',
+ //            text: 'next',
+ //            enabled: true
+ //        });
+       
     }
 
     function onGetTokens(tokens) {
@@ -129,7 +80,8 @@ define([
             eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
             console.log(">>>Event Definition Key " + eventDefinitionKey);
             /*If you want to see all*/
-            console.log('>>>Request Trigger',JSON.stringify(eventDefinitionModel));
+            console.log('>>>Request Trigger', 
+            JSON.stringify(eventDefinitionModel));
         }
 
     });
@@ -137,28 +89,11 @@ define([
     var entrySchema;
     connection.trigger('requestSchema');
     connection.on('requestedSchema', function (data) {
-       // save schema, retrieve field attributes
+       // save schema
        console.log('*** Schema ***', JSON.stringify(data['schema']));
        entrySchema = data['schema'];
-       for(var i = 0; i < entrySchema.length; i++) {
-            var fld = entrySchema[i];
-            var fieldval = JSON.stringify(fld.key).replaceAll('"','');
-            var fieldname = fieldval.split('.')[2];
-            var fieldType = JSON.stringify(fld.type).replaceAll('"','');
-            console.log('Debug fieldname ', fieldname);
-            console.log('Debug fieldType ', fieldType);
-            fieldArr.push(fieldname);
-        }
-        $("input[name='optArr']").val(fieldArr);
-        console.log("Fields=="+JSON.stringify($("input[name='optArr']").val()));
     });
-
-    //trigger JB, and retrieve its information
-    connection.trigger('requestInteraction');
-    connection.on('requestedInteraction', function(interaction) {
-        console.log("interaction==>"+JSON.stringify(interaction));
-    });
-
+    
     String.prototype.replaceAll = function (FindText, RepText) {
         var regExp = new RegExp(FindText, "g");
         return this.replace(regExp, RepText);
@@ -166,103 +101,37 @@ define([
  
     function save() {
 
-        var isRemovedPath = $('#chekcboxId').is(':checked');
-        console.log('customActivity Save function');
-        //Create new path
-        var pathCount = parseInt($("#pathCount").val());
-        console.log("save pathCount=="+pathCount);
-        if(isRemovedPath==true){
-            //remove path
-            payload['outcomes'].splice(pathCount,1);
-            payload['arguments'].execute.inArguments[1].pathCount=pathCount;
-        }
-        else{
-            console.log("CustomActivity pathCount=="+pathCount);
-            payload['arguments'].execute.inArguments.push({"pathCount":$("#pathCount").val()});
+        console.log('do save function.');
 
-            var externalArr =[]; 
-            for(var i=0;i<pathCount;i++){
-                var externalJSON = {};
-                var index = pathCount-i;
-                var inputFieldId = "#val"+index;
-                var inputValue = $(inputFieldId).val();
-                var operatorFieldId = "#operator"+index;
-                var operatorValue = $(operatorFieldId).val();
-                var fieldId = "#sel"+index;
-                var fieldSelectValue = $(fieldId).val();
-                var inputLabelId = "#vLabel"+index;
-                var InputLabelValue = $(inputLabelId).val();
-                //create path Information
-                var brandResultValue = "<KEY FOR Test "+index+">";
-                var brandResult = {"branchResult": brandResultValue };
-                var labelValue = InputLabelValue;
-                var label = {"label": labelValue };
-                console.log("argument:"+JSON.stringify(brandResult));
-                console.log("metaData:"+JSON.stringify(label));
-                var pathInfo = {
-                    "arguments":brandResult,
-                    "metaData":label
-                }
-                payload['outcomes'].push(pathInfo);
-                console.log("add path==>"+index);
-                externalJSON.Field = fieldSelectValue;
-                externalJSON.operator = operatorValue;
-                externalJSON.Input = inputValue;
-                externalJSON.pathName = InputLabelValue;
-                externalJSON.pathIndex = index;
-                externalJSON.BrandResult = brandResultValue;
-                externalJSON.PathCount = pathCount;
-                externalArr.push(externalJSON);
-            }
-
-            var entrySourceJSON = {};
-            entrySourceJSON.External = externalArr;
-            console.log("Before entrySourceJSON.External==>"+JSON.stringify(entrySourceJSON.External));
-            for(var i = 0; i < entrySchema.length; i++) {
-                var fld = entrySchema[i];
-                console.log('cx debug fld', JSON.stringify(fld));
-                var fieldval = JSON.stringify(fld.key).replaceAll('"','');
-                var fieldname = fieldval.split('.')[2];
-                console.log('cx debug fieldname ', fieldname);
-                console.log('cx debug fieldval ', fieldval);
-                entrySourceJSON[fieldname]="{{"+fieldval+"}}";
-            }
-            console.log("EntrySource JSON==>"+JSON.stringify(entrySourceJSON));
-            //payload['arguments'].execute.inArguments.push({[fieldname]: "{{"+fieldval+"}}" });
-            payload['arguments'].execute.inArguments.push(entrySourceJSON);
-            console.log("Save InArgument==>"+JSON.stringify(payload['arguments'].execute.inArguments));
-
-            //array reverse
-            console.log("Before Reserve");
-            console.log("outcomes==>"+JSON.stringify(payload['outcomes']));
-            payload['outcomes'].reverse();
-            console.log("After Reserve");
-            console.log("outcomes==>"+JSON.stringify(payload['outcomes']));
-        }
+        var dateTimeSetting = $("#dateTimelocal").val();
+/*
+        payload['arguments'].execute.inArguments = [{
+            "tokens": authTokens,
+            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+        }];
+        */
+        payload['arguments'].execute.inArguments.push({"dateTimeSetting":dateTimeSetting });
+        
+        //
+        //payload['arguments'].execute.inArguments.push({"CustomerName": "{{Event." + eventDefinitionKey+".CustomerName}}" });
+ 
+        //
+        // for(var i = 0; i < entrySchema.length; i++) {
+        //     var fld = entrySchema[i];
+        //     console.log('cx debug fld', JSON.stringify(fld));
+        //     var fieldval = JSON.stringify(fld.key).replaceAll('"','');
+        //     var fieldname = fieldval.split('.')[2];
+        //     console.log('cx debug fieldname ', fieldname);
+        //     console.log('cx debug fieldval ', fieldval);
+        //     payload['arguments'].execute.inArguments.push({fieldname: fieldval });
+        // }
+         
+        
         payload['metaData'].isConfigured = true;
+
+        console.log(payload);
         connection.trigger('updateActivity', payload);
     }
 
-
-    function initOperation(payLoadTemp){
-        var hasInArguments = Boolean(
-            payLoadTemp['arguments'] &&
-            payLoadTemp['arguments'].execute &&
-            payLoadTemp['arguments'].execute.inArguments &&
-            payLoadTemp['arguments'].execute.inArguments.length > 0
-        );
-
-        var inArguments = hasInArguments ? payLoadTemp['arguments'].execute.inArguments : {};
-        //init UI data form
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
-                console.log("init 1 key==>"+key);
-                console.log("init 1 val==>"+val);
-            });
-        });
-    }
-
-    function createHTML(num){
-    }
 
 });
--- routes/activity.js
+++ routes/activity.js
@@ -7,47 +7,9 @@ const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 // var util = require('util');
 // var http = require('https');
-var schedule = require('node-schedule');
-
-//connect postgreSql
-var pgOpt = require('pg');
-var pgConfig = {
-	user: 'ldqtsrlnhklkwm',
-    database: 'd3dvc63s4josrb',
-    password: '3165f7e45987e7ec447054650fab84a15194a75b6283b1c5bdb5a3f809894d9e',
-    host: 'ec2-75-101-212-64.compute-1.amazonaws.com',
-    port: '5432',
-    poolSize: 5,
-    poolIdleTimeout: 30000,
-    reapIntervalMillis: 10000
-};
-var pgPool = new pgOpt.Pool(pgConfig);
 
 //must install this module
 var request = require('request');
-
-var tokenRequestData={
-"grant_type": "client_credentials",
-"client_id": "pes9dm03ov1ec39rpdeed71o",
-"client_secret": "zmg1PH3zPiFi0C7j3SOjJGAc"
-};
-
-var scheduleJobRetry=0;
-// var requestData={
-// 	"items": []
-// };
-
-
-var retrieveTokenUrl = "https://mcjhmstdw76qsk9kk6m1zpwm4xf4.auth.marketingcloudapis.com/v2/token";
-var insertDEUrl = "https://mcjhmstdw76qsk9kk6m1zpwm4xf4.rest.marketingcloudapis.com/data/v1/async/dataextensions/key:CustomActivity_TargetedAd_DEV/rows";
-
-var isStarScheduleJob = false;
-// const { Client } = require('pg');
-// const client = new Client({
-// 	  connectionString: process.env.DATABASE_URL,
-// 	  ssl: true,
-// 	});
-// client.connect();
 
 exports.logExecuteData = [];
 
@@ -116,9 +78,9 @@ exports.stop = function (req, res) {
  */
 exports.save = function (req, res) {
     console.log('save module');
-	JWT(req.body, 'Hello world', (err, decoded) => {
-		console.log( "decoded==>"+JSON.stringify(  decoded  ));
-	});
+	// JWT(req.body, 'Hello world', (err, decoded) => {
+	// 	console.log( "decoded==>"+JSON.stringify(  decoded  ));
+	// });
     res.status(200).send('Save');
 };
 
@@ -127,8 +89,6 @@ exports.save = function (req, res) {
  */
 exports.execute = function (req, res) {
 	console.log('execute module');
-    let keyVal = req.body.interactionId;
-    console.log("execute interactionid==>"+keyVal);
     // example on how to decode JWT
     JWT(req.body, 'Hello world', (err, decoded) => {
 
@@ -140,74 +100,17 @@ exports.execute = function (req, res) {
 
 
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-        	isStarScheduleJob = true;
-            var map = {};
-            map.runningStartDate="";
-            map.runningEndDate="";
 
             // decoded in arguments
             var decodedArgs = decoded.inArguments[0];            
 
-			// console.log("decodedArgs==>" +JSON.stringify(  decodedArgs  ));
+			//console.log("decodedArgs==>" +JSON.stringify(  decodedArgs  ));
 			console.log( "decoded==>"+JSON.stringify(  decoded  ));
 			console.log("inArguments==>"+JSON.stringify(  decoded.inArguments  ));
-			// console.log("journeyId==>"+JSON.stringify(decoded.journeyId));
-			map.journeyid = decoded.journeyId;
 			
-			for(var i in decoded.inArguments){
-				var adCode = decoded.inArguments[i].ADCode;
-				var startDate = decoded.inArguments[i].startDate;
-				var endDate = decoded.inArguments[i].endDate;
-				var LoyaltyID = decoded.inArguments[i].LoyaltyID;
-				var LocationGroup = decoded.inArguments[i].LocationGroup;
-				var AdPosition = decoded.inArguments[i].AdPosition;
-				var RankedValue = decoded.inArguments[i].RankedValue;
-				var duration = decoded.inArguments[i].Duration;
-
-				if(adCode!=null){
-					map.ADCode = adCode;
-				}
-				else if(startDate !=null){
-					map.startDate = startDate!=""?startDate.replace('T',' '):"";
-				}
-				else if(endDate !=null){
-					map.endDate = endDate!=""?endDate.replace('T',' '):"";
-				}
-				else if(LoyaltyID !=null){
-					map.LoyaltyID = LoyaltyID;
-				}
-				else if(LocationGroup !=null){
-					map.LocationGroup = LocationGroup;
-				}
-				else if(AdPosition !=null){
-					map.AdPosition = AdPosition;
-				}
-				else if(RankedValue !=null){
-					map.RankedValue = RankedValue;
-				}
-				else if(duration !=null){
-					map.duration = duration;
-				}
-			}
-			var isEmpty = JSON.stringify(map)=="{}";
-			if(isEmpty!=true){
-                console.log("map.duration==>"+(map.duration));
-                console.log("map.duration!=''==>"+(map.duration!=""));
-                console.log("map.duration!=0==>"+(map.duration!="0"));
-                if(map.duration!="" && map.duration!="0"){
-                    map.status = 'pending';
-                    var queryStr = 'INSERT INTO ben.input(startdate,enddate,adcode,journeyid,status,createdate,loyaltyid,adposition,rankedvalue,locationgroup,runningstartdate,runningenddate,targetstartdate,targetenddate) VALUES($1::varchar,$2::varchar,$3::varchar,$4::varchar,$5::varchar,$6::varchar,$7::varchar,$8::varchar,$9::varchar,$10::varchar,$11::varchar,$12::varchar,$13::varchar,$14::varchar)';
-                    //
-                    let durationTemp = map.duration == "" ? '0' : map.duration;
-                    map.runningStartDate = convertToLocalDateTime(new Date());
-                    map.runningEndDate = addDays(new Date(),durationTemp,map.startDate);
-
-                    var targetDateTime = retrieveTargetDate(map.runningStartDate,map.runningEndDate,map.startDate,map.endDate);
-
-                    var parameters = [map.startDate,map.endDate,map.ADCode,map.journeyid,map.status,dateFormat(new Date()),map.LoyaltyID,map.AdPosition,map.RankedValue,map.LocationGroup,map.runningStartDate,map.runningEndDate,targetDateTime.TargetStartDate,targetDateTime.TargetEndDate];
-                    insertDataIntoDB(queryStr,parameters);
-                }
-			}
+			// for(var i in decoded.inArguments){
+				
+			// }
 	
             res.status(200).send('Excute');
             //Then , it will update the status to success
@@ -229,12 +132,6 @@ exports.publish = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
     console.log('publish module');
-    let keyVal = req.body.interactionId;
-    console.log("publich interactionid==>"+keyVal);
-    var rule = '0 0/5 * * * *';
-    console.log("rule==>"+rule);
-    scheduleJobRetry = 0;
-    setScheduleJob(rule,retrieveDataFromDB);
     res.status(200).send('Publish');
 };
 
@@ -251,272 +148,5 @@ exports.resolveToken = function (req, res) {
  
 };
 
-function retrieveAccessToken(url,data,deData,deUrl){
-	request({
-        url: url,
-        method: "POST",
-        json: true,
-        headers : {
-			"Content-type" : "application/JSON"
-		},
-        body: tokenRequestData
-    }, function(error, response, body) {
-        if (!error) {
-            //console.log(body) // 请求成功的处理逻辑
-            //console.log('dedata==>'+JSON.stringify(deData));
-            console.log("deURL==>"+deUrl);
-            request({
-    			url: deUrl,
-    			method: "PUT",
-    			json: true,
-    			headers : {
-					"Content-type" : "application/JSON",
-					"Authorization" : "Bearer "+body.access_token
-				},
-    			body: deData
-    			}, function(error, response, body) {
-    				if(error){
-    					console.log("has error");
-    					console.log("response==>"+JSON.stringify(response));
-    					console.log("body==>"+JSON.stringify(body));
-    				}
-    				else{
-    					console.log('done deData==>'+JSON.stringify(deData));
-    					var targetRecords = deData.items;
-    					console.log("target data len==>"+targetRecords.length);
-    					//console.log("targetRecords==>"+JSON.stringify(targetRecords));
-    					var len = targetRecords.length-1;
-    					if(targetRecords.length > 0){
-    						updateRecordsStatus(targetRecords[len].id);
-    					}
-    				}
-    			}
-    		);
-        }
-        else{
-        	console.log("retrieve token error==>"+error);
-        }
-    });
-}
 
-function updateRecordsStatus(id){
-	pgPool.connect(function (isErr, client, done) {
-		if (isErr) {
-			console.log('connect query:' + isErr.message);
-			return;
-		}
-		else{
-			console.log("last index==>"+id);
-			//call producure
-			client.query("CALL ben.updatestatus($1);",[parseInt(id)],function(isErr, rst){
-				if(isErr){
-					console.log('call proc error:' + isErr.message);
-				}	
-				else{ 
-					console.log("call proc successfully");
-				}
-			});
-			client.release();
-		}
-	});
-}
-
-function retrieveDataFromDB(insertDataIntoDE){
-	var reuslt = '';
-	var dateStr = dateFormat(new Date());
-	console.log("dateStr==>"+dateStr);
-	console.log('retrieveDataFromDB');
-	pgPool.connect(function (isErr, client, done) {
-    	console.log("start connection");
-        if (isErr) {
-            console.log('connect query:' + isErr.message);
-            return;
-        }
-        client.query("select id,targetstartdate,targetenddate,adcode,journeyid,status,createdate,loyaltyid,adposition,rankedvalue,locationgroup from ben.input where status !='success' and createdate <=$1 order by id asc", [dateStr], function (isErr, rst) {
-            done();//释放连接，归还给连接池
-            if (isErr) {
-                console.log('retrieve from db query error:' + isErr.message);
-            } 
-            else {
-                //console.log('query success, data is: ' + JSON.stringify(rst.rows));
-                reuslt = rst.rows;
-                var data = rst.rows;
-                //console.log("get data-->"+JSON.stringify(reuslt));
-                var requestData={
-					"items": []
-				};
-				for(var key in data){
-					var resultMap = {};
-				
-					resultMap.id = data[key].id;
-					
-					resultMap.LoyaltyID = data[key].loyaltyid;
-					resultMap.TargetedAdStartDate =data[key].targetstartdate;
-					resultMap.TargetedAdEndDate =data[key].targetenddate;
-					resultMap.ModifiedDate = new Date();
-					resultMap.TargetedAdCode = data[key].adcode;
-					resultMap.AdPosition =data[key].adposition;
-					resultMap.RankedValue =data[key].rankedvalue;
-					resultMap.StoresLocationGroup = data[key].locationgroup;
-
-					requestData.items[key] = resultMap;
-				}
-				var temp = requestData.items;
-				var len = temp.length;
-				if(len ==0){
-					console.log("no data to retrieve");
-				}
-				else{
-					console.log("requestData==>"+JSON.stringify(requestData));
-					console.log("enter insertDE operation");
-                	insertDataIntoDE(insertDEUrl,requestData,retrieveAccessToken);
-				}
-            }
-        });
-    });
-}
-
-function insertDataIntoDE(url,data,retrieveAccessToken){
-	//retrieve access token
-	retrieveAccessToken(retrieveTokenUrl,tokenRequestData,data,url);
-    
-}
-
-function insertDataIntoDB(queryStr,parameters){
-	pgPool.connect(function (isErr, client){
-		if (isErr) {
-    		console.log('connect query:' + isErr.message);
-    		return;
-		}
-		//['testn','testp']
-		//'INSERT INTO ben.input(name,email) VALUES($1::varchar, $2::varchar)'
-		client.query(queryStr, parameters, function (isErr, rst) {
-    		//done();//释放连接，归还给连接池
-    		if (isErr) {
-        		console.log('execute query error:' + isErr.message);
-    		} else {
-        		//console.log('insert success!! ');
-    		}
-		});
- 		client.release();
-	});
-}
-
-function setScheduleJob(rule,retrieveDataFromDB){
-	console.log("start scheduleJob");
-	var j = schedule.scheduleJob(rule,function(){
-		console.log("schedule Job Starting");
-		
-		if(scheduleJobRetry>=3){
-			console.log("stop schedule");
-			console.log("stop database server connection");
-			j.cancel();
-		}
-		else{
-			if(isStarScheduleJob==true){
-				console.log("do something");
-				retrieveDataFromDB(insertDataIntoDE);
-			}
-		}
-	});
-}
-
-function dateFormat(date){
-    var y = date.getFullYear();
-    var m = (date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
-    var d = date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate();
-    var h = date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours();
-    var f = date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes();
-    var sec = date.getSeconds() < 10 ? ('0' + date.getSeconds()) : date.getSeconds();
-    var formatdate = y+'-'+m+'-'+d + " " + h + ":" + f+":"+sec;
-    //console.log("activity formdate===>"+formatdate);
-    return formatdate;
-}
-
-function convertToLocalDateTime(usDate){
-    var localDate = +usDate + 1000*60*60*8;
-    console.log("localDate==>"+new Date(localDate));
-    var date = new Date(localDate);
-
-    var y = date.getFullYear();
-    var m = (date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
-    var d = date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate();
-    var h = date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours();
-    var f = date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes();
-    //var sec = date.getSeconds() < 10 ? ('0' + date.getSeconds()) : date.getSeconds();
-    var formatdate = y+'-'+m+'-'+d + " " + h + ":" + f;
-    console.log("convertToLocalDateTime formdate===>"+formatdate);
-    return formatdate;
-}
-
-function addDays(usDate,duration,startDate){
-	var date;
-	var i = parseInt(duration);
-	if(startDate!=''){
-    	var targetDate = +(new Date(startDate)) + 1000*60*60*24*i;
-    	date = new Date(targetDate);
-	}
-	else{
-		var localDate = +usDate + 1000*60*60*8;
-    	var targetDate = +localDate + 1000*60*60*24*i;
-    	date = new Date(targetDate);
-	}
-
-	var y = date.getFullYear();
-	var m = (date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : (date.getMonth() + 1);
-	var d = date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate();
-	var h = date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours();
-	var f = date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes();
-	//var sec = date.getSeconds() < 10 ? ('0' + date.getSeconds()) : date.getSeconds();
-	var formatdate = y+'-'+m+'-'+d + " " + h + ":" + f;
-	console.log("addDays formdate===>"+formatdate);
-	return formatdate;
-
-}
-
-function retrieveTargetDate(runningStartDate,runningEndDate,originalStartDate,originalEndDate){
-    var result = {};
-    console.log("runningStartDate=="+runningStartDate);
-    console.log("runningEndDate=="+runningEndDate);
-    console.log("originalStartDate=="+originalStartDate);
-    console.log("originalEndDate=="+originalEndDate);
-    
-    if(originalStartDate=="" && originalEndDate==""){
-        result.TargetStartDate = runningStartDate;
-        result.TargetEndDate = runningEndDate.substring(0,10)+" 23:59";
-    }
-    
-    if(originalStartDate!="" && originalEndDate!=""){
-        result.TargetStartDate = originalStartDate;
-        if(runningEndDate > originalEndDate){
-            result.TargetEndDate = originalEndDate
-        }
-        else{
-            result.TargetEndDate = runningEndDate.substring(0,10)+" 23:59";
-        }
-    }
-    
-    if(originalStartDate=="" && originalEndDate!=""){
-        if(runningStartDate >= originalEndDate){
-            result.TargetStartDate = originalEndDate;
-        }
-        else{
-            result.TargetStartDate = runningStartDate;
-        }
-        
-        if(runningEndDate >= originalEndDate){
-            result.TargetEndDate = originalEndDate;
-        }
-        else{
-            result.TargetEndDate = runningEndDate.substring(0,10)+" 23:59";
-        }
-    }
-    
-    if(originalStartDate!="" && originalEndDate==""){
-        result.TargetStartDate = originalStartDate;
-        result.TargetEndDate = runningEndDate.substring(0,10)+" 23:59";
-    }
-    
-    return result;
-}
 
